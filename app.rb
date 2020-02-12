cf68b430987e62b57c242e4551e4d5562f793b3b
FelizKatus <felizkatus@gmail.com>
2020-02-12 01:21:49 +0100

    Init database

--- app.rb
+++ app.rb
@@ -3,10 +3,27 @@ require 'sinatra/reloader'
 require 'sqlite3'
 require 'pony'
 
+def init_db
+  db = SQLite3::Database.new 'db.sqlite'
+  db.results_as_hash = true
+  db
+end
+
+before do
+  init_db
+end
+
 get '/' do
   erb :index
 end
 
 get '/new' do
   erb :new
+end
+
+post '/new' do
+  title   = params[:title]
+  content = params[:content]
+
+  erb "#{title} <br> #{content}"
 end
