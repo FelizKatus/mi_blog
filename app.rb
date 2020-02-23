4f95e86af71c504534e467ca8b84d4ef61b042ae
FelizKatus <felizkatus@gmail.com>
2020-02-23 12:05:45 +0100

    Add GET and POST methods for contact page

--- app.rb
+++ app.rb
@@ -120,3 +120,44 @@ post '/comments/:id' do
 
   redirect to('/comments/' + id)
 end
+
+get '/contact' do
+  erb :contact
+end
+
+post '/contact' do
+  @name    = params[:name]
+  @email   = params[:email]
+  @subject = params[:subject]
+  @message = params[:message]
+
+  hh = {:name    => 'Su nombre',
+        :email   => 'Su email',
+        :subject => 'Su subject',
+        :message => 'su message'}
+
+  hh.each do |key, value|
+    if params[key] == ''
+      @error = hh[key]
+      return erb :contact if @error != ''
+    end
+  end
+
+  Pony.mail(
+    :from    => params[:name] + "<" + params[:email] + ">",
+    :to      => 'username@example.com',
+    :subject => params[:name] + " está comunicando",
+    :body    => params[:message],
+    :via     => :smtp,
+    :via_options => { 
+      :address              => 'smtp.gmail.com', 
+      :port                 => '587', 
+      :enable_starttls_auto => true, 
+      :user_name            => 'username', 
+      :password             => 'password', 
+      :authentication       => :plain, 
+      :domain               => 'localhost.localdomain'
+    })
+
+  erb '¡Gracias! Email sido enviado.'
+end
