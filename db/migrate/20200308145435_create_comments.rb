e32cf1d2f7a62feb80db5ca3b11ce3ed91b06483
FelizKatus <felizkatus@gmail.com>
2020-03-08 15:58:30 +0100

    Add create_comments migration

---
+++ db/migrate/20200308145435_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :comments do |t|
+      t.integer :post_id
+      t.text :name
+      t.text :content
+
+      t.timestamp
+    end
+  end
+end
